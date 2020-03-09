7456f30305e460bdd6a6dd4f03bda9bbfc7ee87b
Wilson de Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-03-09 17:31:42 -0300

    Implementação do grafico por coluna

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,17 +24,45 @@ export class DashboardComponent implements OnInit {
       {
         label: 'Receitas',
         data: [4,10,18,5,1,20,3],
+        fill: false,
         backgroundColor: ['#3366CC']
       },
       {
         label: 'Despesas',
         data: [10,15,8,5,1,7,9],
+        fill: true,
         backgroundColor: ['#D62B00']
       }
 
     ],
 
   };
+
+
+  colunaChartData = {
+    labels: ['Janeiro', 'Fevereiro', 'Março'],
+    datasets: [
+        {
+            label: 'Setor O',
+            backgroundColor: '#42A5F5',
+            borderColor: '#1E88E5',
+            data: [65, 59, 80]
+        },
+        {
+            label: 'Aguas Claras',
+            backgroundColor: '#9CCC65',
+            borderColor: '#7CB342',
+            data: [28, 48, 40]
+        },
+        {
+          label: 'Planaltina',
+          backgroundColor: 'red',
+          borderColor: 'red',
+          data: [28, 48, 40]
+        }
+
+    ]
+}
 
   constructor() { }
 
