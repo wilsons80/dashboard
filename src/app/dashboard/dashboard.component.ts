5f8c5af976c8fd85c3b805b8bbbf0620411c8222
Wilson de Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-03-12 19:23:37 -0300

    Grafico apenas com Chartjs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -68,6 +68,10 @@ export class DashboardComponent implements OnInit {
   @ViewChild("myChart")
   public refChart: ElementRef;
 
+  @ViewChild("myChart2")
+  public refChart2: ElementRef;
+
+
   public chartData: any;
 
   constructor() {
@@ -117,5 +121,51 @@ export class DashboardComponent implements OnInit {
             }
         }
     });
+
+
+    this.carregarGrafico();
+  }
+
+
+
+
+  carregarGrafico() {
+    var ctx = document.getElementById('myChart2');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
   }
 }
