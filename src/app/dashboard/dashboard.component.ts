ffc8a1ec2f43083f4fcacf3f66bacee3ec3fdb45
Wilson de Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-03-12 19:10:16 -0300

    Grafico apenas com Chartjs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'dashboard',
@@ -64,9 +65,57 @@ export class DashboardComponent implements OnInit {
     ]
 }
 
-  constructor() { }
+  @ViewChild("myChart")
+  public refChart: ElementRef;
 
-  ngOnInit(): void {
+  public chartData: any;
+
+  constructor() {
+    this.chartData = {};
+   }
+
+  public ngOnInit() {
+    this.chartData = {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+}
+
+  public ngAfterViewInit() {
+    let chart = this.refChart.nativeElement;
+    let ctx = chart.getContext("2d");
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: this.chartData,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
   }
-
 }
